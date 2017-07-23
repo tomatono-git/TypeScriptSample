3ef30d4f7f8b9b780d66ba10fd2acbcd72dce9d3
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2017-07-23 20:59:49 +0900

    jstreeをインストール

--- SampleTypeahead/typings/index.d.ts
+++ SampleTypeahead/typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/jstree/index.d.ts" />
 /// <reference path="globals/typeahead/index.d.ts" />
 /// <reference path="modules/knockout.mapping/index.d.ts" />
 /// <reference path="modules/knockout/index.d.ts" />
