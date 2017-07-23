073488e76ea55828e1d90c3f05a186329e0b6a16
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2017-07-23 20:54:57 +0900

    knockout.mappingsのtypingsをinstall

--- SampleTypeahead/typings/index.d.ts
+++ SampleTypeahead/typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/typeahead/index.d.ts" />
+/// <reference path="modules/knockout.mapping/index.d.ts" />
 /// <reference path="modules/knockout/index.d.ts" />
